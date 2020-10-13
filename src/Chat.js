6911378b6cb7bf437a4cf3331ee7778c9283298b
andrewhopgood <andrewhopgood@rogers.com>
2020-10-13 10:06:28 -0400

    completed sidebar

---
+++ src/Chat.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Chat.css";
+
+function Chat() {
+  return (
+    <div className="chat">
+      <h2>Chat screen</h2>
+    </div>
+  );
+}
+
+export default Chat;
